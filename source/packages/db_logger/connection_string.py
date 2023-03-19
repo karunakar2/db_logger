e5e3bd48decf02854ed4b13996cadee1a88cc3f8
Sourcery AI <>
2023-03-19 21:13:55 +0000

    'Refactored by Sourcery'

--- source/packages/db_logger/connection_string.py
+++ source/packages/db_logger/connection_string.py
@@ -1,13 +1,7 @@
 from os import environ
 
-name = environ.get('name')
-if name:
-    name = name.lower()
-else:
-    name = 'sqlite'
-
-driver = environ.get('driver')
-if driver:
+name = name.lower() if (name := environ.get('name')) else 'sqlite'
+if driver := environ.get('driver'):
     driver = driver.lower()
 database = environ.get('database')
 if not database:
@@ -23,16 +17,16 @@ def create_connection_string():
     # dialect+driver://username:password@host:port/database
     connection_string = f'+{driver}://{username}:{password}@{host}:{port}/{database}'
     if name in ("postgresql",):
-        return "postgresql" + connection_string
+        return f"postgresql{connection_string}"
 
     elif name in ('mysql', ):
-        return 'mysql' + connection_string
+        return f'mysql{connection_string}'
 
     elif name in ('oracle', ):
-        return "oracle" + connection_string
+        return f"oracle{connection_string}"
 
     elif name in ('microsoft sql server', 'mssql'):
-        return "mssql" + connection_string
+        return f"mssql{connection_string}"
 
     elif name in ("sqlite", 'sqlite3'):
         # sqlite://<nohostname>/<path>
